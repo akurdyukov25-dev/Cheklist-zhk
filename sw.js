3159cf5488715c19643cc6d846d5ff72fb097f9a
akurdyukov25-dev <a.kurdyukov25@gmail.com>
2026-05-18 18:09:27 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE = 'checklist-zhk-v1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(FILES);
+    }).then(function() {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); })
+      );
+    }).then(function() { return clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      if(cached) return cached;
+      return fetch(e.request).then(function(response) {
+        if(response && response.status === 200 && response.type !== 'opaque') {
+          var clone = response.clone();
+          caches.open(CACHE).then(function(cache){ cache.put(e.request, clone); });
+        }
+        return response;
+      }).catch(function() { return cached; });
+    })
+  );
+});
